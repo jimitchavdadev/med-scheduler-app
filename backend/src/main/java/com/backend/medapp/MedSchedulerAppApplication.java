30bee48600d3300fe2d11f853af2bdd4e18ce513
Jimit Chavda <jimitchavdadev@gmail.com>
2025-10-03 23:53:39 +0530

    basic project

---
+++ backend/src/main/java/com/backend/medapp/MedSchedulerAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.medapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSchedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSchedulerAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/medapp/MedSchedulerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.medapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSchedulerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
